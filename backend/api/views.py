e3bb57ef8c2249c12bd63fa8f3c57e6a63625f80
root <root@Felix>
2023-04-30 18:49:28 +0700

    Rest framework view and response

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,15 +3,41 @@ from django.http import JsonResponse
 from django.forms.models import model_to_dict
 from products.models import Product
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
+    """
+    DRF API View
+    """
     model_data = Product.objects.all().order_by("?").first() # get random product
     data = {}
 
     if model_data:
         data = model_to_dict(model_data, fields=['id', 'title', 'price']) # serialization
-    return JsonResponse(data)
+    return Response(data)
 
-# last checkpoint : rest framework view & response
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # def api_home(request, *args, **kwargs):
     # Request query params
